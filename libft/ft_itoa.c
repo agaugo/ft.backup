704bf0c6da579cde6bf22cb838a705370e3ffe79
Hugo Flohil De Sousa <hflohil-@f1r6s2.codam.nl>
2022-10-25 10:41:32 +0200

    vibes

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_strtrim.c                                       :+:    :+:            */
+/*   ft_itoa.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: hflohil- <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/20 13:41:52 by hflohil-      #+#    #+#                 */
-/*   Updated: 2022/10/20 13:41:53 by hflohil-      ########   odam.nl         */
+/*   Created: 2022/10/22 19:32:49 by hflohil-      #+#    #+#                 */
+/*   Updated: 2022/10/22 19:32:50 by hflohil-      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*buffer;
-
-	buffer = malloc(sizeof(char const) * ft_strlen(s1));
-	if (!buffer)
-		return (0);
-	return (buffer);
-}
-
-int	main(void)
-{
-	printf("$%s$\n", ft_strtrim("lorem ipsum dolor sit amet", "te"));
-	return (0);
-}
+char *ft_itoa(int n);
