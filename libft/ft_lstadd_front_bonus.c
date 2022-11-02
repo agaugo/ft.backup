356d74293f992aecc338f34c6a54a33e9c15d052
Hugo Flohil de Sousa <cd@example.com>
2022-11-02 15:49:12 +0100

    bonus

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -12,15 +12,8 @@
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*node;
-
-	node = (t_list *)malloc(sizeof(t_list));
-	if(!node)
-		return (NULL);
-	node->content = content;
-	node->next = NULL;
-
-	return (node);
+	new->next = *lst;
+	*lst = new;
 }
