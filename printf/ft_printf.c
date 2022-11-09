518c67e59feb88c21592da136c6fd2a6862808c3
Hugo Flohil de Sousa <cd@example.com>
2022-11-09 10:23:06 +0100

    printf

---
+++ printf/ft_printf.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_tolower.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: hflohil- <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/06 15:12:58 by hflohil-      #+#    #+#                 */
+/*   Updated: 2022/10/06 15:17:13 by hflohil-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+// *#include "printf.h"
+#include <stdio.h>
+
+int	*ft_printf(const char *input, ...)
+{
+	return ();
+}
+
+int	main(void)
+{
+	printf("%s", ft_printf("steep", 3));
+	return 0;
+}
