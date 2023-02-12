3a738d62b8588eaf2a02451ab0397a2897591533
Hugo Flohil de Sousa <cd@example.com>
2023-02-12 13:54:36 +0100

    first commit

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: hflohil- <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/06 15:12:58 by hflohil-      #+#    #+#                 */
+/*   Updated: 2022/10/06 15:17:13 by hflohil-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+- Repeated calls (e.g., using a loop) to your get_next_line() function should let you read the text file pointed to by the file descriptor, one line at a time.
+- Your function should return the line that was read.
+- If there is nothing else to read or if an error occurred, it should return NULL.
+- Make sure that your function works as expected both when reading a file and when reading from the standard input.
+- Please note that the returned line should include the terminating \n character, except if the end of file was reached and does not end with a \n character.
+- Your header file get_next_line.h must at least contain the prototype of the get_next_line() function.
+- Add all the helper functions you need in the get_next_line_utils.c file
+*/
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+
+}
+
+int	main(void)
+{
+	get_next_line("123456789\n123456789");
+	return (0);
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: hflohil- <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/06 15:12:58 by hflohil-      #+#    #+#                 */
+/*   Updated: 2022/10/06 15:17:13 by hflohil-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdint.h>
+
+char	*get_next_line(int fd);
+
+#endif
