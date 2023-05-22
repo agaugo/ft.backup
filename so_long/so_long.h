e1ddf3bef7fb5ed33fc6a38d74d26682b95d3cc8
Hugo Flohil De Sousa <hflohil-@f0r1s3.codam.nl>
2023-05-22 20:53:19 +0200

    finally fixe dstupid bug:

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,17 +1,23 @@
-#include "mlx.h"
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+
+# include "mlx/mlx.h"
+# include <fcntl.h>
 
 // **** STRUCTS **** //
 
 # define IMG_SIZE 32
 
 typedef	struct	s_data {
+	void	*mlx;
+	void	*win;
 	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
 }	t_data;
 
 //  **** CORE **** //
 
 int		main(void);
 void	pixel_put(t_data	*data, int x, int y, int color);
+
+#endif
--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -1,26 +1,16 @@
-#include "so_long.h"
+#include "../so_long.h"
 #include <stdio.h>
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	// // char	*path = "banana.xpm";
-	void	*img;
-	// // t_data	img;
-
+	t_data	game;
 	int wd;
 	int hh;
 
-	printf("prinbtyedfsfssaaf");
-	mlx = mlx_init();
-	printf("prinbtyedfsfssaaf");
-	mlx_win = mlx_new_window(mlx, 10 * IMG_SIZE, 10 * IMG_SIZE, "so_long");
-	printf("prinbtyedfsfssaaf");
-	img = mlx_xpm_file_to_image(mlx, "player.xpm", &wd, &hh);
-	printf("prinbtyed");
-	mlx_put_image_to_window(mlx, mlx_win, &img, 0 * IMG_SIZE, 0 * IMG_SIZE);
-	printf("prinbtye3423424242d");
-	mlx_loop(mlx_win);
+	game.mlx = mlx_init();
+	game.win = mlx_new_window(game.mlx, (10*IMG_SIZE), (10*IMG_SIZE), "TEST");
+	game.img = mlx_xpm_file_to_image(game.mlx, "./test.xpm", &wd, &hh);
+	mlx_put_image_to_window(game.mlx, game.win, game.img, (0 * IMG_SIZE), (0 * IMG_SIZE));
+	mlx_loop(game.mlx);
 	return (0);
 }
